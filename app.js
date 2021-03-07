ef40849ea768f4314e703b3ea9dd2aa973c2d9f2
Damir Mustafin <damir.mustafin@protonmail.ch>
2021-03-07 21:18:03 +0300

    add server

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const Koa = require('koa')
+const serve = require('koa-static')
+const cors = require('@koa/cors')
+
+const app = new Koa()
+
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 8080
+
+app.use(cors())
+app.use(serve(path.resolve('dist')))
+
+app.listen(port, host)

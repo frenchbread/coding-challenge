fcd696e3312caeb76d51d9baccad08e8f83136f6
Damir Mustafin <damir.mustafin@protonmail.ch>
2021-03-07 21:40:50 +0300

    cleanup & bump version

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ const cors = require('@koa/cors')
 
 const app = new Koa()
 
-const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 8080
 
 app.use(cors())
 app.use(serve(path.resolve('dist')))
 
-app.listen(port, host)
-
-console.log(`${host} ${port}`)
+app.listen(port)
